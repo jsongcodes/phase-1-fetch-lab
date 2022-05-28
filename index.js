cabedaf77bfb09312cd58ee1002104c42584c5de
Jasmin <ssongnari@gmail.com>
2022-05-28 09:28:55 -0400

    commit tree 8112e3af9a4f04fd53257189dcf47eada2b08da5

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks();
+});
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
@@ -8,10 +13,7 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+    main.appendChild(h2)
+  })
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+}
